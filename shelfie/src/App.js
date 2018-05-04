37114aa950d95091f7231ef0c7b9c33e985843f9
Russell Memory <rmemory@Russells-MacBook-Pro-2.local>
2018-05-04 12:22:04 -0600

    step 4 complete

--- shelfie/src/App.js
+++ shelfie/src/App.js
@@ -4,6 +4,7 @@ import Dashboard from './component/Dashboard/Dashboard'
 import Form from './component/Form/Form'
 import Product from './component/Product/Product'
 import Header from './component/Header/Header'
+import axios from 'axios'
 
 // let inventory = ;
 
@@ -13,25 +14,19 @@ class App extends Component {
     super()
 
     this.state = {
-      inventoryList: [
-        {
-          name: "shoes",
-          price: 10,
-          image: 'www.shoes.com'
-        },
-        {
-          name: "shirt",
-          price: 20,
-          image: 'www.shirt.com'
-        },
-        {
-          name: "pants",
-          price: 30,
-          image: 'www.pants.com'
-        }
-      ]
+      inventoryList: []
     }
   }
+
+  componentDidMount(){
+    axios.get('/api/inventory').then(res => {
+      console.log(res);
+      this.setState({
+        inventoryList: res.data
+      })
+    }).catch(e => {console.log("this is the error", e)})
+  }
+
 
   render() {
     return (
@@ -47,3 +42,20 @@ class App extends Component {
 }
 
 export default App;
+
+
+// {
+//   name: "shoes",
+//   price: 10,
+//   image: 'www.shoes.com'
+// },
+// {
+//   name: "shirt",
+//   price: 20,
+//   image: 'www.shirt.com'
+// },
+// {
+//   name: "pants",
+//   price: 30,
+//   image: 'www.pants.com'
+// }
